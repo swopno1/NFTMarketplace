a1b8871bc70f132f4f3434e5644a8a93573c4bdf
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-06-24 23:14:46 +0600

    Added card and more component.

--- components/FocusedStatusBar.js
+++ components/FocusedStatusBar.js
@@ -1,12 +1,10 @@
-import { View, Text, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useIsFocused } from '@react-navigation/core';
 
-const FocusedStatusBar = () => {
-  return (
-    <View>
-      <Text>FocusedStatusBar</Text>
-    </View>
-  );
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 };
 
 export default FocusedStatusBar;
--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,10 +1,42 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+import { CircleButton } from '../components';
+import { SubInfo } from './SubInfo';
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>NFTCard</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark,
+      }}
+    >
+      <View
+        style={{
+          width: '100%',
+          height: 250,
+        }}
+      >
+        <Image
+          source={data.image}
+          resizeMode='cover'
+          style={{
+            width: '100%',
+            height: '100%',
+            borderBottomLeftRadius: SIZES.font,
+            borderBottomRightRadius: SIZES.font,
+          }}
+        />
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
+      </View>
+      <SubInfo />
     </View>
   );
 };
