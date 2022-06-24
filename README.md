da877048564d56c815bac4ce62beb948727d9bfd
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-06-24 18:05:11 +0600

    Added assets, components, and constants folder and components item.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LibanFood - My first React Native application
+
+This is a demo application develop by for NFT Marketplace Course. Though the name is something different.
---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { View, Text, StatusBar } from 'react-native';
+import { useIsFocused } from '@react-navigation/core';
+
+const FocusedStatusBar = () => {
+  return (
+    <View>
+      <Text>FocusedStatusBar</Text>
+    </View>
+  );
+};
+
+export default FocusedStatusBar;
